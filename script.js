12327657423b99d6933a444c007e1d39438c5de5
saksham-accio <125554544+saksham-accio@users.noreply.github.com>
2023-04-15 09:52:59 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 /** @format */
 
 let arr = [
-  { id: 1, name: "john", age: "18", marks: 80 },
-  { id: 2, name: "jack", age: "20", marks: 85 },
-  { id: 3, name: "karen", age: "19", marks: 35 },
+  { id: 1, name: "john", age: "18", profession: "developer" },
+  { id: 2, name: "jack", age: "20", profession: "developer" },
+  { id: 3, name: "karen", age: "19", profession: "admin },
 ];
 
 function PrintStudentswithMap() {
